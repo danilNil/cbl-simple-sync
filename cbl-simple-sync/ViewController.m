a5e1510a512ec9041cce31a16b950345c87e02cd
danilNil <danil.nikiphorov@gmail.com>
2015-03-03 19:58:30 +0600

    added logic for resolving conflicts. !call pull manually did we resolve conflicts. possibly bug in framework

--- cbl-simple-sync/ViewController.m
+++ cbl-simple-sync/ViewController.m
@@ -30,22 +30,23 @@
 -(void)showCurrentPerson{
     currentPerson = [self getPersonFromDB];
     if(currentPerson)
-    self.name.text = currentPerson.name;
+        self.name.text = currentPerson.name;
     self.position.text = currentPerson.position;
 }
 
 - (Person*)getPersonFromDB{
     NSError* error;
-
+    
     CBLView* personsView = [AppDelegate.database viewNamed: @"persons"];
     [personsView setMapBlock: MAPBLOCK({
         emit(doc[@"_id"], nil);
     }) version: @"2"];
     
-
+    
     CBLQuery* personQuery = [personsView createQuery];
     personQuery.keys = @[docId];
     CBLQueryEnumerator* result = [personQuery run:&error];
+    NSLog(@"personQuery run:&error: %@", error);
     Person* model = nil;
     if(result.count>0){
         CBLQueryRow* row = [result nextObject];
@@ -66,14 +67,68 @@
     person.name = self.name.text;
     person.position = self.position.text;
     [person save:&error];
+    NSLog(@"save:&error: %@", error);
 }
 
 - (IBAction)update:(id)sender {
     [self putPersonInDB];
-
+    
 }
 
 - (IBAction)refresh:(id)sender {
     [self showCurrentPerson];
+}
+
+- (IBAction)checkConflicts:(id)sender {
+    CBLDocument* doc = [AppDelegate.database documentWithID: docId];
+    NSError* error;
+    NSArray* conflicts = [doc getConflictingRevisions: &error];
+    if (conflicts.count > 1) {
+        // There is more than one current revision, thus a conflict!
+//        [AppDelegate.database inTransaction: ^BOOL{
+            // Come up with a merged/resolved document in some way that's
+            // appropriate for the app. You could even just pick the body of
+            // one of the revisions.
+            NSDictionary* mergedProps = [self mergeRevisions: conflicts];
+            
+            // Delete the conflicting revisions to get rid of the conflict:
+            CBLSavedRevision* current = doc.currentRevision;
+            for (CBLSavedRevision* rev in conflicts) {
+                NSLog(@"rev prop: %@ is deleted: %i", rev.properties, rev.isDeletion);
+                CBLUnsavedRevision *newRev = [rev createRevision];
+                if (rev == current) {
+                    // add the merged revision
+                    newRev.properties = [NSMutableDictionary dictionaryWithDictionary: mergedProps];
+                } else {
+                    // mark other conflicts as deleted
+                    newRev.isDeletion = YES;
+                }
+
+                NSError *error;
+                if (![newRev saveAllowingConflict: &error]){
+                    NSLog(@"newRev save: &error: %@", error);
+//                    return NO;
+                }
+            }
+//            return YES;
+//        }];
+//        NSError* error;
+        [AppDelegate runSync];
+        [self showCurrentPerson];
+    }
+}
+
+- (NSDictionary*)mergeRevisions:(NSArray*)conflicts{
+    NSMutableDictionary* mergedDict = [[NSMutableDictionary alloc] initWithDictionary:((CBLRevision*)conflicts[0]).properties];
+    NSMutableArray* array = [NSMutableArray arrayWithArray:conflicts];
+    [array removeObjectAtIndex:0];
+    for(CBLRevision* rev in array){
+        for(NSString* key in rev.properties){
+            if(![key isEqualToString:@"_id"] && ![key isEqualToString:@"_rev"] && ![key isEqualToString:@"_revisions"])
+                mergedDict[key] = [NSString stringWithFormat:@"%@ %@", mergedDict[key], rev.properties[key]];
+        }
+    }
+    NSLog(@"final properties to merge: %@", mergedDict);
+    return mergedDict;
 }
 @end
